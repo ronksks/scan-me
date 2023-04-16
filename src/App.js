018e9b49f34b25f561363a6532ad61b5938e0367
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-16 13:41:38 +0300

    2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
 
 function App() {
   const [scannedData, setScannedData] = useState("");
-  const [scannerVisible, setScannerVisible] = useState(false);
 
   const handleScanSuccess = (decodedText) => {
     setScannedData(decodedText);
-    setScannerVisible(false);
+    Html5QrcodeScanner.clear();
+    console.log("Scanned Data:", decodedText);
   };
 
   const handleScanFailure = (errorMessage) => {
     console.error(errorMessage);
   };
 
-  const handleScanClick = () => {
-    setScannerVisible(true);
-  };
-
-  useEffect(() => {
-    let html5QrCodeScanner;
-    if (scannerVisible) {
-      html5QrCodeScanner = new Html5QrcodeScanner(
+  const handleScanClick = async () => {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const html5QrCodeScanner = new Html5QrcodeScanner(
         "html5-qrcode-scanner",
-        { fps: 10, qrbox: { width: 250, height: 250 } }
-        /* useBackCamera: true by default */
+        {
+          fps: 10,
+          qrbox: { width: 250, height: 250 },
+          constraints: { facingMode: "environment" },
+        },
+        /* verbose= */ false
       );
       html5QrCodeScanner.render(handleScanSuccess, handleScanFailure);
+    } catch (err) {
+      console.error(err);
     }
-    return () => {
-      if (html5QrCodeScanner) {
-        html5QrCodeScanner.stop();
-      }
-    };
-  }, [scannerVisible]);
+  };
 
   return (
     <div>
       <button onClick={handleScanClick}>Scan Barcode</button>
       {scannedData && <p>Scanned Data: {scannedData}</p>}
-      {scannerVisible && <div id="html5-qrcode-scanner"></div>}
+      <div id="html5-qrcode-scanner"></div>
     </div>
   );
 }
