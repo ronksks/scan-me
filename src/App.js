43ef658771cb0c5322987cd712144a5e25badb5c
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-16 13:16:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 function App() {
+  const [scannedData, setScannedData] = useState("");
+
+  const handleScanSuccess = (decodedText) => {
+    setScannedData(decodedText);
+    document.getElementById("html5-qrcode-scanner").remove();
+  };
+
+  const handleScanFailure = (errorMessage) => {
+    console.error(errorMessage);
+  };
+
+  const handleScanClick = () => {
+    const html5QrCodeScanner = new Html5QrcodeScanner(
+      "html5-qrcode-scanner",
+      { fps: 10, qrbox: { width: 250, height: 250 } },
+      false
+    );
+    html5QrCodeScanner.render(handleScanSuccess, handleScanFailure);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleScanClick}>Scan Barcode</button>
+      {scannedData && <p>Scanned Data: {scannedData}</p>}
+      <div id="html5-qrcode-scanner"></div>
     </div>
   );
 }
