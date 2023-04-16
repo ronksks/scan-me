44b739e19b69929a9c37d7fee6c75be41fc30d22
Ron <32454051+ronksks@users.noreply.github.com>
2023-04-16 18:40:11 +0300

    render what type of camera set active camera

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 function App() {
   const [scannedData, setScannedData] = useState("");
   const [cameras, setCameras] = useState([]);
+  const [activeCamera, setActiveCamera] = useState(null);
 
   function handleScanClick() {
     // This method will trigger user permissions
@@ -23,6 +24,7 @@ function App() {
           const qrCodeSuccessCallback = (decodedText, decodedResult) => {
             /* handle success */
             setScannedData(decodedText);
+            setActiveCamera(decodedResult.cameraId);
             console.log(scannedData);
             html5QrCode
               .stop()
@@ -76,16 +78,19 @@ function App() {
         {scannedData && <p>Scanned Data: {scannedData}</p>}
         <div id="reader"></div>
       </div>
-      {/* <div>
+      <div>
+        <p>
+          Current Camera:{" "}
+          {activeCamera
+            ? cameras.find((camera) => camera.id === activeCamera)?.label ||
+              "Unknown"
+            : "None"}
+        </p>
         <ul>
           {cameras.map((item) => {
             return <li>{item.label}</li>;
           })}
         </ul>
-      </div> */}
-      <div>
-        Current Camera:{" "}
-        {cameras.length ? cameras[cameras.length - 1].label : "None"}
       </div>
     </div>
   );
@@ -95,49 +100,91 @@ export default App;
 
 //********* this works so far ************ */
 
-// import React, { useState } from "react";
-// // To use Html5QrcodeScanner (more info below)
-// import { Html5QrcodeScanner } from "html5-qrcode";
-
-// // To use Html5Qrcode (more info below)
 // import { Html5Qrcode } from "html5-qrcode";
+// import { useState } from "react";
+
 // function App() {
 //   const [scannedData, setScannedData] = useState("");
+//   const [cameras, setCameras] = useState([]);
 
-//   const handleScanSuccess = (decodedText) => {
-//     setScannedData(decodedText);
-//     // Html5QrcodeScanner.clear();
-//     console.log("Scanned Data:", decodedText);
-//   };
+//   function handleScanClick() {
+//     // This method will trigger user permissions
+//     Html5Qrcode.getCameras()
+//       .then((devices) => {
+//         /**
+//          * devices would be an array of objects of type:
+//          * { id: "id", label: "label" }
+//          */
+//         setCameras(devices);
 
-//   const handleScanFailure = (errorMessage) => {
-//     console.error(errorMessage);
-//   };
+//         if (devices && devices.length) {
+//           console.log("entered if (devices && devices.length)");
+//           var cameraId = devices[1].id;
+//           // .. use this to start scanning.
+//           const html5QrCode = new Html5Qrcode("reader");
+//           const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+//             /* handle success */
+//             setScannedData(decodedText);
+//             console.log(scannedData);
+//             html5QrCode
+//               .stop()
+//               .then((ignore) => {
+//                 // QR Code scanning is stopped.
+//               })
+//               .catch((err) => {
+//                 // Stop failed, handle it.
+//               });
+//           };
+//           const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
-//   const handleScanClick = async () => {
-//     try {
-//       // const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-//       const html5QrCodeScanner = new Html5QrcodeScanner(
-//         "html5-qrcode-scanner",
-//         {
-//           fps: 10,
-//           qrbox: { width: 250, height: 250 },
-//           constraints: { facingMode: "environment" },
-//         },
-//         /* verbose= */ false
-//       );
-//       html5QrCodeScanner.render(handleScanSuccess, handleScanFailure);
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
+//           // ************  Back Camera hardcoded
+
+//           html5QrCode
+//             .start(
+//               { facingMode: { exact: "environment" } },
+//               config,
+//               qrCodeSuccessCallback
+//             )
+//             .then(cameras.push({ id: "10", labeL: "enviorment" }))
+//             .catch((err) => {
+//               // ************  Back Camera
+//               html5QrCode
+//                 .start(
+//                   { deviceId: { exact: cameraId } },
+//                   config,
+//                   qrCodeSuccessCallback
+//                 )
+//                 .then(cameras.push({ id: "11", labeL: "rear_camera" }));
+//             });
+
+//           // // ************  Back Camera
+//           // html5QrCode.start(
+//           //   { deviceId: { exact: cameraId } },
+//           //   config,
+//           //   qrCodeSuccessCallback
+//           // );
+//         }
+//       })
+//       .catch((err) => {
+//         // if(err.text == "OverconstrainedError" )
+//         // handle err
+//       });
+//   }
 
 //   return (
 //     <div>
-//       <button onClick={handleScanClick}>Scan Barcode</button>
-//       {scannedData && <p>Scanned Data: {scannedData}</p>}
-//       <div id="html5-qrcode-scanner"></div>
-//       {/* <div id="reader"></div> */}
+//       <div>
+//         <button onClick={handleScanClick}>Scan Barcode</button>
+//         {scannedData && <p>Scanned Data: {scannedData}</p>}
+//         <div id="reader"></div>
+//       </div>
+//       <div>
+//         <ul>
+//           {cameras.map((item) => {
+//             return <li>{item.label}</li>;
+//           })}
+//         </ul>
+//       </div>
 //     </div>
 //   );
 // }
